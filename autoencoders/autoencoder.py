e81c838b87cc21ec85f9f5bb4c765e39b50d7984
synstratos <luca.spartera93@gmail.com>
2020-05-22 21:09:07 +0200

    updated kernel initializers

--- autoencoders/autoencoder.py
+++ autoencoders/autoencoder.py
@@ -3,11 +3,9 @@ import tensorflow as tf
 import math
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras import Model
-from sklearn.preprocessing import MinMaxScaler
 
 
 class AE(Model):
-
     def __init__(self,
                  n_input=None,
                  code_nodes=None,
@@ -15,25 +13,45 @@ class AE(Model):
                  _code_activation='relu',
                  _output_activation='sigmoid',
                  _inner_activation='tanh',
+                 _code_kernel='he_uniform',
+                 _output_kernel='truncated_normal',
+                 _inner_kernel='glorot_uniform',
                  _red_factor=7.5,
+                 _n_layers=3,
                  *args,
                  **kwargs):
+        """
+        :param n_input: number of input features. If you are using a DataFrame, it will be equal to df.shape[1]
+        :param code_nodes: number of required nodes for the encoder output. It will be set automatically depending on
+        the reduction factor if not given by the user.
+        :param summary: set to True if you want to print the AutoEncoder model structure.
+        :param _code_activation: activation function at the code layer. Set to ReLu by default.
+        :param _output_activation: activation function at the output layer. Set to Sigmoid by default.
+        :param _inner_activation: activation function at Encoder and Decoder inner layers. Set to TanH by default.
+        :param _code_kernel: kernel initializer at the code layer. Set to He Uniform by default.
+        :param _output_kernel: kernel initializer at the output layer. Set to Truncated Normal by default.
+        :param _inner_kernel: kernel initializer at Encoder and Decoder inner layers. Set to Glorot Uniform by default.
+        :param _red_factor: reduction factor used to choose the number of code neurons if not explicitly given.
+        :param _n_layers:
+        :param args:
+        :param kwargs:
+        """
 
         # data = np.array(data)
         # data = __standardize__(data)
-        self.input_nodes = n_input #data.shape[0]
+        self.input_nodes = n_input  # data.shape[0]
 
         # either a number of code nodes or reduction factor can be specified
         self.code_nodes = __even__(self.input_nodes / _red_factor) if not code_nodes else code_nodes
 
-        # number of layers -> how to define
-        n_layers = 5
+        # number of layers -> TODO: choose how to define
+        n_layers = _n_layers
 
+        # CHOSEN WAY TO SELECT CORRECT NUMBER OF LAYER NODES, BASED ON LOGSPACE
         a = np.log(self.code_nodes)
         b = np.log(self.input_nodes)
 
         encoder_layers = np.flip(np.logspace(a, b, n_layers, base=np.e))[1:-1]
-
         decoder_layers = np.logspace(a, b, n_layers, base=np.e)[1:-1]
 
         _LAYERS = []
@@ -44,50 +62,39 @@ class AE(Model):
 
         for n in encoder_layers:
             _LAYERS.append(
-                Dense(__even__(n), activation = _inner_activation,)(_LAYERS[-1])
+                Dense(__even__(n), activation=_inner_activation, kernel_initializer=_inner_kernel)(_LAYERS[-1])
             )
 
-        self.code_layer = Dense(self.code_nodes, activation = _code_activation)(_LAYERS[-1])
+        self.code_layer = Dense(self.code_nodes, activation=_code_activation, kernel_initializer=_code_kernel)(_LAYERS[-1])
         _LAYERS.append(self.code_layer)
 
         for n in decoder_layers:
             _LAYERS.append(
-                Dense(__even__(n), activation = _inner_activation,)(_LAYERS[-1])
+                Dense(__even__(n), activation=_inner_activation, kernel_initializer=_inner_kernel)(_LAYERS[-1])
             )
 
-        output_layer = Dense(self.input_nodes, activation=_output_activation)(_LAYERS[-1])
+        output_layer = Dense(self.input_nodes, activation=_output_activation, kernel_initializer=_output_kernel)(_LAYERS[-1])
 
         # SETUP MODEL
         super().__init__(self.input_layer, output_layer, *args, **kwargs)
 
+        # PRINT OUT MODEL STRUCTURE IF REQUIRED
         if summary:
             self.summary()
 
-    # def compile_fit(self, ):
-    #     self.
-
     def generate_encoder(self):
+        """
+        Creates the encoder model extracting it from the trained Autoencoder.
+        :return:
+        """
         encoder = Model(self.input_layer, self.code_layer)
         return encoder
 
 
-def __standardize__(data):
-    indexes = []
-    for col in range(data.shape[1]):
-        if min(data[:, col]) < 0 or max(data[:, col]) > 1:
-            indexes.append(col)
-
-    if len(indexes) > 0:
-        to_scale = data[:, indexes]
-        not_to_scale = np.delete(data, indexes, axis=1)
-        scaled = MinMaxScaler().fit_transform(to_scale)
-
-        minmaxdata = np.concatenate((scaled, not_to_scale), axis = 1)
-        return minmaxdata
-    else:
-        return data
-
-
 def __even__(f):
+    """
+    Help function to round each number to even.
+    :param f:
+    :return:
+    """
     return math.ceil(f / 2.) * 2
-
--- autoencoders/main.py
+++ autoencoders/main.py
@@ -1,4 +1,6 @@
 from autoencoders.autoencoder import AE
 import numpy as np
-a = AE(data = np.zeros((100, 100)), summary=True)
-
+a = AE(n_input = 1000, code_nodes = 800, summary=False, _n_layers= 3)
+# a.fit()
+encoder = a.generate_encoder()
+encoder.summary()
