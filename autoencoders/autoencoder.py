486d14d652f1a29680a3d34ee2947632ef134527
synstratos <luca.spartera93@gmail.com>
2020-05-24 20:07:54 +0200

    added a possible function to calculate the number of layers

--- autoencoders/autoencoder.py
+++ autoencoders/autoencoder.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 import math
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras import Model
@@ -17,7 +16,6 @@ class AE(Model):
                  _output_kernel='truncated_normal',
                  _inner_kernel='glorot_uniform',
                  _red_factor=7.5,
-                 _n_layers=3,
                  *args,
                  **kwargs):
         """
@@ -32,20 +30,17 @@ class AE(Model):
         :param _output_kernel: kernel initializer at the output layer. Set to Truncated Normal by default.
         :param _inner_kernel: kernel initializer at Encoder and Decoder inner layers. Set to Glorot Uniform by default.
         :param _red_factor: reduction factor used to choose the number of code neurons if not explicitly given.
-        :param _n_layers:
         :param args:
         :param kwargs:
         """
 
-        # data = np.array(data)
-        # data = __standardize__(data)
-        self.input_nodes = n_input  # data.shape[0]
+        self.input_nodes = n_input
 
         # either a number of code nodes or reduction factor can be specified
         self.code_nodes = __even__(self.input_nodes / _red_factor) if not code_nodes else code_nodes
 
-        # number of layers -> TODO: choose how to define
-        n_layers = _n_layers
+        # number of layers
+        n_layers = __layer_number__(n_input, code_nodes)
 
         # CHOSEN WAY TO SELECT CORRECT NUMBER OF LAYER NODES, BASED ON LOGSPACE
         a = np.log(self.code_nodes)
@@ -54,26 +49,26 @@ class AE(Model):
         encoder_layers = np.flip(np.logspace(a, b, n_layers, base=np.e))[1:-1]
         decoder_layers = np.logspace(a, b, n_layers, base=np.e)[1:-1]
 
-        _LAYERS = []
+        LAYERS = []
 
         # GENERATE LAYERS
         self.input_layer = Input(shape=(self.input_nodes,))
-        _LAYERS.append(self.input_layer)
+        LAYERS.append(self.input_layer)
 
         for n in encoder_layers:
-            _LAYERS.append(
-                Dense(__even__(n), activation=_inner_activation, kernel_initializer=_inner_kernel)(_LAYERS[-1])
+            LAYERS.append(
+                Dense(__even__(n), activation=_inner_activation, kernel_initializer=_inner_kernel)(LAYERS[-1])
             )
 
-        self.code_layer = Dense(self.code_nodes, activation=_code_activation, kernel_initializer=_code_kernel)(_LAYERS[-1])
-        _LAYERS.append(self.code_layer)
+        self.code_layer = Dense(self.code_nodes, activation=_code_activation, kernel_initializer=_code_kernel)(LAYERS[-1])
+        LAYERS.append(self.code_layer)
 
         for n in decoder_layers:
-            _LAYERS.append(
-                Dense(__even__(n), activation=_inner_activation, kernel_initializer=_inner_kernel)(_LAYERS[-1])
+            LAYERS.append(
+                Dense(__even__(n), activation=_inner_activation, kernel_initializer=_inner_kernel)(LAYERS[-1])
             )
 
-        output_layer = Dense(self.input_nodes, activation=_output_activation, kernel_initializer=_output_kernel)(_LAYERS[-1])
+        output_layer = Dense(self.input_nodes, activation=_output_activation, kernel_initializer=_output_kernel)(LAYERS[-1])
 
         # SETUP MODEL
         super().__init__(self.input_layer, output_layer, *args, **kwargs)
@@ -85,16 +80,35 @@ class AE(Model):
     def generate_encoder(self):
         """
         Creates the encoder model extracting it from the trained Autoencoder.
-        :return:
+        :return: keras model of the encoder
         """
         encoder = Model(self.input_layer, self.code_layer)
         return encoder
 
 
+def __layer_number__(a, b):
+    """
+    Calculates the proper number of hidden layers
+    :param a: number of input nodes
+    :param b: number of code nodes
+    :return: number of layers from input to code
+    """
+    if a < b:
+        raise Exception("The number of code neurons must be lower then input nodes number.")
+    div = a / b
+
+    if div > 1.5:
+        div = math.log(div, 10)
+        div = math.e**div
+        div += 1
+
+    return math.ceil(div)
+
+
 def __even__(f):
     """
     Help function to round each number to even.
-    :param f:
-    :return:
+    :param f: given number
+    :return: even approximation of the given number
     """
     return math.ceil(f / 2.) * 2
--- autoencoders/main.py
+++ autoencoders/main.py
@@ -1,6 +1,6 @@
 from autoencoders.autoencoder import AE
-import numpy as np
-a = AE(n_input = 1000, code_nodes = 800, summary=False, _n_layers= 3)
+
+a = AE(n_input = 80, code_nodes = 10, summary=False)
 # a.fit()
 encoder = a.generate_encoder()
 encoder.summary()
